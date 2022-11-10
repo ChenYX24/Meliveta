9aa63b5cc13d6694acf59c1da0b34d9bfb445e81
ChenYX24 <90974567+ChenYX24@users.noreply.github.com>
2022-11-10 09:50:22 +0800

    局域网摄像头

---
+++ loom-main/src/server.mjs
@@ -0,0 +1,43 @@
+// 後端實作程式碼
+
+import express from "express"
+import http from "http"
+import { Server } from "socket.io"
+
+const app = express()
+const server = http.createServer(app)
+const io =new Server(server)
+
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.get("/", (_, res) => {
+  res.render("home")
+})
+
+app.get("/rooms/new",(req,res)=>{
+  res.redirect(`/${Math.floor(Math.random()*1000000000)}`)
+})
+
+app.get('/:room',(req,res)=>{
+  res.render('room',{ room_id: req.params.room})
+})
+
+//websocket
+io.on("connection",(socket)=>{
+  socket.on("ENTER_ROOM",(room_id, user_name)=>{
+    socket.join(room_id)
+    socket.broadcast.to(room_id).emit('USER_ENTERED',user_name)
+    socket.on('disconnect',()=>{
+      socket.broadcast.to(room_id).emit('USER_LEFT',user_name)
+    })
+    socket.on("R_C",(call1,id)=>{
+      socket.broadcast.to(room_id).emit('RETURN_USERS',call1,id)
+      
+    })
+  })
+})
+
+server.listen(3000, () => {
+  console.log(`application is running on http://localhost:3000`)
+})
